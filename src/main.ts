479cbd4e8cc5319df633f8385c96923dfba660e5
rob587 <robertocamm02@yahoo.com>
2026-01-10 14:47:28 +0100

    fix prima milestone- seconda milestone completata

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 type Person = {
   readonly id: number;
-  name: string;
+  readonly name: string;
   birth_year: number;
   death_year?: number;
   biography: string;
   image: string;
+};
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string];
+  awards: string;
+  nationality:
+    | "American"
+    | "British"
+    | "Australian"
+    | "Israeli-American"
+    | "South African"
+    | "French"
+    | "Indian"
+    | "Israeli"
+    | "Spanish"
+    | "South Korean"
+    | "Chinese";
 };
